bcef7c16ed9b620692cc17ee4ced30ca2ffdb45a
ProjectSoft <projectsoft2009@yandex.ru>
2024-08-07 19:23:52 +0400

    update

--- site/assets/snippets/GetEvoVideo/snippet.GetEvoVideo.php
+++ site/assets/snippets/GetEvoVideo/snippet.GetEvoVideo.php
@@ -6,7 +6,7 @@ if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
 $url = isset($url) ? $url : '';
 $vd = new Video(null, false);
 $arr = $vd->setLink($url);
-$type = isset($type) ? $type : 'video';
+$type = isset($type) ? $type : 'link';
 $return = "";
 switch($type) {
 	case 'link':
